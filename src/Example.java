6f419f11964e41acb39e98e7c5d61d54fca9bc1f
voicuz <voicu.z@gmail.com>
2022-06-29 12:56:49 +0300

    Learning Java.

--- src/Example.java
+++ src/Example.java
@@ -2691,8 +2691,6 @@ class SumDemo {
     }
 }
 
- */
-
 // A simple example of recursion.
 class Factorial {
     // This is a recursive function.
@@ -2728,5 +2726,114 @@ class Recursion {
         System.out.println("Factorial of 3 is " + f.factI(3));
         System.out.println("Factorial of 4 is " + f.factI(4));
         System.out.println("Factorial of 5 is " + f.factI(5));
+    }
+}
+
+
+// Use a static variable.
+class StaticDemo {
+    int x;  // a normal instance variable
+    static int y;  // a static variable
+
+    // Return the sum of the instance variable x
+    // and the static variable y.
+    int sum() {
+        return x + y;
+    }
+}
+
+class SDemo {
+    public static void main(String[] args) {
+        StaticDemo ob1 = new StaticDemo();
+        StaticDemo ob2 = new StaticDemo();
+
+        // Each object has its own copy of an instance variable.
+        ob1.x = 10;
+        ob2.x = 20;
+        System.out.println("Of course, ob1.x and ob2.x are independent.");
+        System.out.println("ob1.x: " + ob1.x + "\nob2.x: " + ob2.x);
+        System.out.println(ob1.sum());
+        System.out.println(ob2.sum());
+        System.out.println();
+
+        // Each object shares one copy of the static variable.
+        System.out.println("The static variable y is shared.");
+        StaticDemo.y = 19;
+        System.out.println("Set StaticDemo.y to 19.");
+
+        System.out.println("ob1.sum: " + ob1.sum());
+        System.out.println("ob2.sum: " + ob2.sum());
+        System.out.println();
+
+        StaticDemo.y = 100;
+        System.out.println("Change StaticDemo.y to 100");
+
+        System.out.println("ob1.sum: " + ob1.sum());
+        System.out.println("ob2.sum: " + ob2.sum());
+        System.out.println();
+    }
+}
+
+// Use a static method.
+class StaticMeth {
+    static int val = 1024;  // a static variable
+
+    // a static method
+    static int vlaDiv2() {
+        return val/2;
+    }
+}
+
+class SDemo2 {
+    public static void main(String[] args) {
+
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " + StaticMeth.vlaDiv2());
+
+        StaticMeth.val = 4;
+        System.out.println("val is " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " + StaticMeth.vlaDiv2());
+    }
+}
+
+
+
+// Won't compile
+class StaticError {
+    int denom = 3;  // a normal instance variable
+    static int val = 1024;  // a static variable
+
+    // Error! Can't access a non-static variable from within a static method.
+    static int valDivDenom() {
+        return val/denom;  // won't compile
+    }
+}
+
+ */
+
+
+// Use a static block
+class StaticBlock {
+    static double rootOf2;
+    static double rootOf3;
+
+    static {
+        System.out.println("Inside static block.");
+        rootOf2 = Math.sqrt(2.0);
+        rootOf3 = Math.sqrt(3.0);
+        System.out.println("Still inside static block");
+    }
+
+    StaticBlock(String msg) {
+        System.out.println(msg);
+    }
+}
+
+class SDemo3 {
+    public static void main(String[] args) {
+        StaticBlock ob = new StaticBlock("Inside constructor");
+
+        System.out.println("Square root of 2 is " + StaticBlock.rootOf2);
+        System.out.println("Square root of 3 is " + StaticBlock.rootOf3);
     }
 }
